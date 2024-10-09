24427eff9d000e67edec562b706e944dec8a3f95
Melbanda28 <122828898+Melbanda28@users.noreply.github.com>
2024-10-09 15:15:27 +0000

    added some links

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App">
       <h1>Weather App</h1>
-      </header>
+      <footer>
+        <p>
+          This project was coded by {""}
+          <a href="https://github.com/Melbanda28">Mellania Banda</a>
+       {""} 
+       and is open-sourced
+          on {""}
+          <a href="https://github.com/Melbanda28/react-weather-app">Github</a>
+        </p>
+      </footer>
     </div>
   );
 }
