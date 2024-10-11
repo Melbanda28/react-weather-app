173177ea05a6d8406049decd34eda8816eca7ce3
Melbanda28 <122828898+Melbanda28@users.noreply.github.com>
2024-10-11 12:27:24 +0000

    added basic css and some api features using axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css"
 
 
-export default function Weather () {
-    return(
-        <div className="Weather">
-       <form>
-        <div className="row">
+export default function Weather (props) {
+    
+    const[weatherData, setWeatherData]=useState({ready: false});
+   
+    function handleResponse(response){;
+    console.log(response.data);
+    setWeatherData({
+        ready:true,
+    temperature:response.data.main.temp,
+    wind:response.data.wind.speed,
+    humidity:response.data.main.humidity,
+    date:"Tuesday 1700",
+    description:response.data.weather[0].description,
+    city:response.data.name});
+
+}
+
+if (weatherData.ready){
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
             <div className="col-9">
               <input
                 type="search"
                 placeholder="Enter a city.."
                 className="searchbar w-100"
-                autoFocus="on" 
-            
+                autoFocus="on"
               />
             </div>
             <div className="col-3">
@@ -22,31 +39,40 @@ export default function Weather () {
                 value="Search"
                 className="btn btn-primary w-100"
               />
-              </div>
-              </div>
-       </form>
-        <h1>Cape Town</h1>
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
         <ul>
-          <li>Tuesday:1700</li>
-          <li>Partly cloudy</li>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row">
           <div className="col-6">
             <img
               src="https://assets.msn.com/weathermapdata/1/static/weather/Icons/taskbar_v10/Condition_Card/D200PartlySunnyV2.svg"
-              alt="partly sunny"
+              alt={weatherData.description}
             />
-            <span className="temperature" >16</span> 
+            <span className="temperature">{Math.round(weatherData.temperature)}</span>
             <span className="unit"> ℃</span>
           </div>
           <div className="col-6">
             <ul>
-              <li>humidity:30%</li>
-              <li>precipitation:50%</li>
-              <li>Wind:20km/h</li>
+              <li>humidity:{weatherData.humidity}%</li>
+              <li>Wind:{weatherData.wind}km/h</li>
             </ul>
           </div>
         </div>
       </div>
-    )
+    );
+}
+else{
+    const apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
+    let city= "Cape Town";
+    let apiUrl =
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return"Loading...";
+}
 }
